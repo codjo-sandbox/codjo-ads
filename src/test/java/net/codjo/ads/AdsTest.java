380ad350591d3ac2b95f9c303e43468d4b509599
Fabien DUMINY <fabien.duminy@allianzgi.fr>
2014-10-13 17:38:52 +0200

    Ads test account password change.

--- src/test/java/net/codjo/ads/AdsTest.java
+++ src/test/java/net/codjo/ads/AdsTest.java
@@ -32,7 +32,7 @@ public interface AdsTest {
           .setPermissionBase(PERMISSION_BASE).get();
 
     String LOGIN = "frnormal1";
-    String PASSWORD = "adspwd*17";
+    String PASSWORD = "adspwd*18";
 
     int USER_SECURITY_LEVEL = 1;
     int SERVICE_SECURITY_LEVEL = 0;
